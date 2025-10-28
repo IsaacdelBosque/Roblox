6d9687dc623a3667d2412fd70ccbccdedcd7f6e8
Isaac del Bosque <isaacsosappss@gmail.com>
2025-10-27 19:37:52 -0600

    Update Hola.lua

--- Hola.lua
+++ Hola.lua
@@ -1,6 +1,5 @@
--- whitelist.lua (example for GitHub)
 return {
-    Owner = {3778231239, 7282899615},
-    Mod = {123456789, 987654321},
-    Premium = {111222333, 444555666}
+    Owner = {3778231239},
+    Mod = {},
+    Premium = {123456789}  -- <-- AQUÍ PON EL USERID DE TU CUENTA PREMIUM
 }
